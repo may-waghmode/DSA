fd374bdee6bda98d93410687f3d454619eff86a0
Mayur Waghmode <mayurwaghmode11@gmail.com>
2026-04-10 14:27:22 +0530

    Create LC169.cpp

---
+++ Arrays/LC169.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int ans=0,freq=0;
+        for(int i=0;i<nums.size();i++){
+            if(freq==0){
+                ans = nums[i];
+            }
+            if(nums[i]== ans){
+                freq++;
+            }
+            else{
+                freq--;
+            }
+        }
+        return ans;
+    }
+};
